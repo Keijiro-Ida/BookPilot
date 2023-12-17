0726d0bee4657a5601200d7c9145d12fb74407da
Keijiro-Ida <idatt1122@gmail.com>
2023-12-17 08:01:26 +0000

    vue install

---
+++ resources/views/app.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+
+    <!-- CSRF Token -->
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+
+
+    <title>{{ config('app.name', 'Vue Laravel SPA') }}</title>
+
+
+
+    <!-- Styles -->
+
+
+
+</head>
+
+<body>
+
+<div id="app">
+
+<example-component></example-component>
+
+</div>
+
+<!-- Scripts -->
+
+<head>
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    </head>
+</body>
+
+</html>
