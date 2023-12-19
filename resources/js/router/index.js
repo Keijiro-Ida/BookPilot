589d5cbcd6774a7b7830afb7f533785acdcd6cc2
Keijiro-Ida <idatt1122@gmail.com>
2023-12-19 22:02:44 +0000

    vuex

---
+++ resources/js/router/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import BookShelfComponent from "../components/BookShelfComponent.vue";
+import BookShowComponent from "../components/BookShowComponent.vue";
+import BookReviewComponent from "../components/BookReviewComponent.vue";
+
+const router = new createRouter({
+    history:createWebHistory(),
+    routes: [
+        {
+            path: '/books',
+            name: 'book.shelf',
+            component: BookShelfComponent
+        },
+        {
+            path: '/books/:bookId',
+            name: 'book.show',
+            component: BookShowComponent
+        },
+        {
+            path: '/books/search',
+            name: 'book.search',
+            component: BookShowComponent
+        },
+        {
+            path: '/books/:bookId/review',
+            name: 'book.review',
+            component: BookReviewComponent
+        },
+    ]
+});
+
+export default router;
---
+++ resources/js/store.js
@@ -0,0 +1,7 @@
+import { createStore } from 'vuex';
+
+const store = createStore({
+  // ステート、ミューテーション、アクション、ゲッターなど
+});
+
+export default store;
